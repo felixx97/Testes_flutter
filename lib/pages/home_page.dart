20f6c62893ac4f72806c21922bf19018aaebc85f
felixx97 <abrahammfelixx@gmail.com>
2022-01-31 15:46:10 -0300

    animando bottom pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController = PageController();
+
+  int indexBottomNavigation = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,55 +18,87 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("AppBar dois"),
       ),
-      body: Column(
+      body: PageView(
+        controller: _pageController,
         children: [
-          Container(
-            height: 200,
-            width: MediaQuery.of(context)
-                .size
-                .width, //MQ consegue pegar o tamanho do dispositivo a partir do contexto, pegue o size e o width
-            decoration: const BoxDecoration(
-              color: Colors.red,
-            ),
-            child: const Center(
-                child: Text(
-              "Olá mundinho",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-              ),
-            )),
-          ),
-          Row(
+          Column(
             children: [
               Container(
-                color: Colors.black12,
-                height: 100,
-                width: 200,
+                height: 200,
+                width: MediaQuery.of(context)
+                    .size
+                    .width, //MQ consegue pegar o tamanho do dispositivo a partir do contexto, pegue o size e o width
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: const Center(
+                    child: Text(
+                  "Olá mundinho",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                )),
               ),
-              Container(
-                color: Colors.pink,
-                height: 100,
-                width: 200,
+              Row(
+                children: [
+                  Container(
+                    color: Colors.black12,
+                    height: 100,
+                    width: 200,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    height: 100,
+                    width: 200,
+                    child: Center(
+                      child: Text(
+                        "container 2",
+                        style: TextStyle(color: Colors.purple),
+                      ),
+                    ),
+                  )
+                ],
               )
             ],
-          )
+          ),
+          Container(
+            color: Colors.black,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.cyan,
+          ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.access_alarm),
-          label: "Item 1",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.access_alarm),
-          label: "Item 1",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.adb_sharp),
-          label: "Item 1",
-        ),
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indexBottomNavigation,
+          onTap: (int index) {
+            setState(() {
+              indexBottomNavigation = index; //animando o bottom navegation
+            });
+            _pageController.animateToPage(
+              index,
+              duration: Duration(milliseconds: 300),
+              curve: Curves.ease,
+            );
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm),
+              label: "Item 1",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm),
+              label: "Item 1",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.adb_sharp),
+              label: "Item 1",
+            ),
+          ]),
     );
   }
 }
